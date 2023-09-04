0fca0d96e305dfecd73b310b3210e898a8ac07d0
Earl Potters <earl.potters@gmail.com>
2023-09-04 21:37:38 +0200

    finish component

--- src/components/ProductPage/ProductPage.tsx
+++ src/components/ProductPage/ProductPage.tsx
@@ -239,12 +239,15 @@ export default function Example() {
                             </div>
 
                             <div className="mt-10 flex">
-                                <button
-                                    type="submit"
-                                    className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
-                                >
-                                    Add to bag
-                                </button>
+                                <a href="https://www.amazon.com" target="_blank" rel="noopener noreferrer">
+
+                                    <button
+                                        type="button"
+                                        className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
+                                    >
+                                        Export to Amazon
+                                    </button>
+                                </a>
 
                                 <button
                                     type="button"
--- src/pages/index.js
+++ src/pages/index.js
@@ -66,12 +66,22 @@ const HomePage = () => {
             </div>
           </div> */}
         </div>
-
+        {/* 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-5 py-4">
           <ProductCard />
           <ProductCard />
           <ProductCard />
           <ProductCard />
+        </div> */}
+
+        <div className="relative items-start gap-20 mt-[70px] md:mt-5 md:flex-col py-4">
+          <div className="mt-10">
+            <h2 className="text-3xl font-semibold mb-5">Featured Backpacks</h2>
+
+
+            <CartPage />
+
+          </div>
         </div>
       </div>
     </>
